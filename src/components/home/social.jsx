6dc72fe2fa2b0f2422425259766769fc70c4a93a
dewank07 <dewankrastogi007@gmail.com>
2023-07-03 19:15:20 +0530

    fix: build error empty herf

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -4,15 +4,15 @@ import "./home.css"
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="#" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="#" className="home__social-icon" target="_blank">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a href="" className="home__social-icon" target="_blank">
+        <a href="#" className="home__social-icon" target="_blank">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
