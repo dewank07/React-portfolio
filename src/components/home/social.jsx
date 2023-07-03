b4e65ce914eaabeab53fa03748838b02bb28f068
dewank07 <dewankrastogi007@gmail.com>
2023-07-03 19:23:27 +0530

    fix: build error herf edited

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -4,15 +4,15 @@ import "./home.css"
 const Social = () => {
   return (
     <div className="home__social">
-        <a className="home__social-icon" target="_blank">
+        <a href="https://dewank.social/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a className="home__social-icon" target="_blank">
+        <a href="https://dewank.social/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-instagram"></i>
         </a>
 
-        <a className="home__social-icon" target="_blank">
+        <a href="https://dewank.social/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
